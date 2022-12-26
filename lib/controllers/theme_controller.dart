af0520015a357eba5aae5c38ba53e4a6b82129da
ezequiasmelo <ezequias-rm@hotmail.com>
2022-12-26 10:22:04 -0300

    add dark theme with sharedpreferences

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:hive/hive.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
@@ -17,14 +13,8 @@ class ThemeController extends GetxController {
   static ThemeController get to => Get.find();
 
   loadThemeMode() async {
-    // with SharedPreferences
     prefs = await SharedPreferences.getInstance();
     String themeText = prefs!.getString('theme') ?? 'light';
-
-    // with Hive
-    // Directory dir = await getApplicationDocumentsDirectory();
-    // var box = await Hive.openBox('preferencias', path: dir.path);
-    // String themeText = box.get('theme') ?? 'light';
 
     isDark.value = themeText == 'dark' ? true : false;
     setMode(themeText);
@@ -34,13 +24,8 @@ class ThemeController extends GetxController {
     ThemeMode? themeMode = themeModes[themeText];
     Get.changeThemeMode(themeMode!);
 
-    // with SharedPreferences
     prefs = await SharedPreferences.getInstance();
     await prefs!.setString('theme', themeText);
-
-    // with Hive
-    // var box = await Hive.openBox('preferencias');
-    // await box.put('theme', themeText);
   }
 
   changeTheme() {
